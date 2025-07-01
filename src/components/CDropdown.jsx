3d27f2ee93091428c6aea31e6dd892eb959c5d2d
DevShayan <45141872+DevShayan@users.noreply.github.com>
2025-07-01 13:16:30 +0500

    Changes to be committed: 	modified:   src/components/CDropdown.jsx

--- src/components/CDropdown.jsx
+++ src/components/CDropdown.jsx
@@ -15,7 +15,7 @@ export default function CDropdown({ text, innerText }) {
         <p className="text-[20px] font-[800] leading-[120%] sm:text-[32px]">{ text }</p>
         { isExpanded && <p className="text-[14px] sm:text-[24px] leading-[150%] mt-[16px] text-[#CBD5E1]">{ innerText }</p> }
       </div>
-      <div onClick={ toggleExpanded } className="size-[40px] sm:size-[80px] flex justify-center items-center shadow-[inset_0px_-3px_4px_rgba(168,162,255,0.5),inset_0px_3px_4px_rgba(168,162,255,0.5)] backdrop-blur-[127px] rounded-full cursor-pointer ml-[16px] shrink-0">
+      <div onClick={ toggleExpanded } className="size-[40px] sm:size-[80px] flex justify-center items-center shadow-[inset_0px_-3px_4px_rgba(168,162,255,0.5),inset_0px_3px_4px_rgba(168,162,255,0.5)] backdrop-blur-[127px] rounded-full cursor-pointer ml-[16px] shrink-0 self-start">
         { isExpanded
           ? <Minus className="w-[10px] sm:w-[22px]" />
           : <Plus className="w-[10px] sm:w-[22px]" /> }
